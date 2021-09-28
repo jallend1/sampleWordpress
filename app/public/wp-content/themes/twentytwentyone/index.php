601abe1d09f2a93f1637f09f889eb310c046c283
jallend1 <jallend1@gmail.com>
2021-09-27 20:57:09 -0700

    Begins setting up new theme

--- app/public/wp-content/themes/twentytwentyone/index.php
+++ app/public/wp-content/themes/twentytwentyone/index.php
@@ -15,7 +15,6 @@
  */
 
 get_header(); ?>
-
 <?php if ( is_home() && ! is_front_page() && ! empty( single_post_title( '', false ) ) ) : ?>
 	<header class="page-header alignwide">
 		<h1 class="page-title"><?php single_post_title(); ?></h1>
